c703a37d28c0122b05004f1c81795d74edc76949
Prashant <pk021998@gmail.com>
2017-11-12 00:46:11 +0530

    12-11-17

--- app/src/main/java/com/cbs/sscbs/Gallery_Activity.kt
+++ app/src/main/java/com/cbs/sscbs/Gallery_Activity.kt
@@ -3,13 +3,10 @@ package com.cbs.sscbs
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-/**
- * Created by gauti on 28/10/17.
- */
 class Gallery_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.gallery_activity)
+        setContentView(R.layout.activity_gallery_)
     }
 }
