9e9459b3792c9d169fccbb3db0cc72a0c0c54cae
Prashant <pk021998@gmail.com>
2017-11-18 05:30:31 +0530

    Gallery

--- app/src/main/java/com/cbs/sscbs/Gallery_Activity.kt
+++ app/src/main/java/com/cbs/sscbs/Gallery_Activity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.ViewFlipper
+import com.bumptech.glide.Glide
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_gallery_.*
 
@@ -23,58 +25,22 @@ class Gallery_Activity : AppCompatActivity() {
         setContentView(R.layout.activity_gallery_)
         setToolbar()
 
-
         viewFlipper = this.findViewById<ViewFlipper>(R.id.flipper1) as ViewFlipper
-        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
-        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
-        viewFlipper.setInAnimation(fade_in)
-        viewFlipper.setOutAnimation(fade_out)
-        viewFlipper.setAutoStart(true)
-        viewFlipper.setFlipInterval(1500)
-        viewFlipper.startFlipping()
+        showImage(R.id.flipper1, "infrastructure", 2)
 
         viewFlipper = this.findViewById<ViewFlipper>(R.id.flipper2) as ViewFlipper
-        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
-        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
-        viewFlipper.setInAnimation(fade_in)
-        viewFlipper.setOutAnimation(fade_out)
-        viewFlipper.setAutoStart(true)
-        viewFlipper.setFlipInterval(1500)
-        viewFlipper.startFlipping()
-
+        showImage(R.id.flipper2, "events", 3)
 
         viewFlipper = this.findViewById<ViewFlipper>(R.id.flipper3) as ViewFlipper
-        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
-        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
-        viewFlipper.setInAnimation(fade_in)
-        viewFlipper.setOutAnimation(fade_out)
-        viewFlipper.setAutoStart(true)
-        viewFlipper.setFlipInterval(1500)
-        viewFlipper.startFlipping()
-
+        showImage(R.id.flipper3, "society", 3)
 
         viewFlipper = this.findViewById<ViewFlipper>(R.id.flipper4) as ViewFlipper
-        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
-        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
-        viewFlipper.setInAnimation(fade_in)
-        viewFlipper.setOutAnimation(fade_out)
-        viewFlipper.setAutoStart(true)
-        viewFlipper.setFlipInterval(1500)
-        viewFlipper.startFlipping()
-
+        showImage(R.id.flipper4, "victories", 3)
 
         viewFlipper = this.findViewById<ViewFlipper>(R.id.flipper5) as ViewFlipper
-        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
-        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
-        viewFlipper.setInAnimation(fade_in)
-        viewFlipper.setOutAnimation(fade_out)
-        viewFlipper.setAutoStart(true)
-        viewFlipper.setFlipInterval(1500)
-        viewFlipper.startFlipping()
-
+        showImage(R.id.flipper5, "sports", 3)
 
     }
-
 
     fun setToolbar() {
         setSupportActionBar(toolbar_gallery)
@@ -85,19 +51,35 @@ class Gallery_Activity : AppCompatActivity() {
         }
     }
 
-    fun showImage() {
+    private fun showImage(id: Int, folder: String, noOfPics: Int) {
+
+        for (i in 0 until noOfPics)
+        {
         firebasedb = FirebaseDatabase.getInstance()
-        firebaseref = firebasedb.getReference("timetable/0")
+        firebaseref = firebasedb.getReference("${folder}/${i}")
+
         firebaseref.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onDataChange(p0: DataSnapshot?) {
                 val url: String? = p0?.getValue(String::class.java)
-
+                setImageInFlipr(findViewById(id), url)
             }
-        })
-    }
+        })}
 }
+    private fun setImageInFlipr(frame: ViewFlipper, imgUrl: String?) {
 
+        val image = ImageView(applicationContext)
+        Glide.with(this).load(imgUrl).into(image)
+        frame.addView(image)
+        fade_in = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
+        fade_out = AnimationUtils.loadAnimation(this, android.R.anim.fade_out)
+        frame.setInAnimation(fade_in)
+        frame.setOutAnimation(fade_out)
+        frame.setAutoStart(true)
+        frame.setFlipInterval(1500)
+        frame.startFlipping()
+    }
+
+}
--- app/src/main/java/com/cbs/sscbs/SampleWEB.java
+++ app/src/main/java/com/cbs/sscbs/SampleWEB.java
@@ -1,81 +1,81 @@
-package com.cbs.sscbs;
-
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
-
-import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
-import org.w3c.dom.Document;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class SampleWEB extends AppCompatActivity {
-
-
-    WebView wv;
-    Handler uiHandler = new Handler();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sample_web);
-
-        wv = (WebView) findViewById(R.id.sampleWeb);
-        wv.getSettings().setJavaScriptEnabled(false);
-        new LoadData().execute();
-    }
-
-    private class  LoadData extends AsyncTask<Void,Void,Void>
-    {
-        String primeDiv="div2";
-        String html=new String();
-        Document doc = null;
-        @Override
-        protected Void doInBackground(Void... params) {
-
-            try {
-
-                doc = (Document) Jsoup.connect("http://10.0.2.2/foodbookdhaka/webview.html").timeout(100000).get();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //get total document
-
-            Elements alldivs=doc.select("div");
-            ArrayList<String> list=new ArrayList<String>();
-
-            for(org.jsoup.nodes.Element e: alldivs)
-            {
-                if(!e.id().equals(""))
-                    list.add(e.id());
-            }
-            //removing all <div> without "div2"
-            for(int i=0;i<list.size();i++)
-            {
-                if(!list.get(i).equals(primeDiv))
-                    doc.select("div[id="+list.get(i)+"]").remove();
-            }
-
-            html=alldivs.outerHtml();
-
-            return null;
-        }
-        @Override
-        protected void onPostExecute(Void result) {
-
-            super.onPostExecute(result);
-            wv.loadDataWithBaseURL(null,doc.,
-                    "text/html", "utf-8", null);
-//            String summary = "<html><body>You scored <b>192</b> points.</body></html>";
-//            wv.loadData(summary, "text/html", null);
-
-        }
-    }
-
-
-}
-
+//package com.cbs.sscbs;
+//
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+//import android.webkit.WebView;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.select.Elements;
+//import org.w3c.dom.Document;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+//public class SampleWEB extends AppCompatActivity {
+//
+//
+//    WebView wv;
+//    Handler uiHandler = new Handler();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_sample_web);
+//
+//        wv = (WebView) findViewById(R.id.sampleWeb);
+//        wv.getSettings().setJavaScriptEnabled(false);
+//        new LoadData().execute();
+//    }
+//
+//    private class  LoadData extends AsyncTask<Void,Void,Void>
+//    {
+//        String primeDiv="div2";
+//        String html=new String();
+//        Document doc = null;
+//        @Override
+//        protected Void doInBackground(Void... params) {
+//
+//            try {
+//
+//                doc = (Document) Jsoup.connect("http://10.0.2.2/foodbookdhaka/webview.html").timeout(100000).get();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            //get total document
+//
+//            Elements alldivs=doc.select("div");
+//            ArrayList<String> list=new ArrayList<String>();
+//
+//            for(org.jsoup.nodes.Element e: alldivs)
+//            {
+//                if(!e.id().equals(""))
+//                    list.add(e.id());
+//            }
+//            //removing all <div> without "div2"
+//            for(int i=0;i<list.size();i++)
+//            {
+//                if(!list.get(i).equals(primeDiv))
+//                    doc.select("div[id="+list.get(i)+"]").remove();
+//            }
+//
+//            html=alldivs.outerHtml();
+//
+//            return null;
+//        }
+//        @Override
+//        protected void onPostExecute(Void result) {
+//
+//            super.onPostExecute(result);
+//            //wv.loadDataWithBaseURL(null,doc.,
+//                    "text/html", "utf-8", null);
+////            String summary = "<html><body>You scored <b>192</b> points.</body></html>";
+////            wv.loadData(summary, "text/html", null);
+//
+//        }
+//    }
+//
+//
+//}
+//
