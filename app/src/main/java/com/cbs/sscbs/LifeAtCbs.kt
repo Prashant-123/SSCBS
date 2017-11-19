d5a3b40150d14a0e2828f8ae3a9a046167a4a257
Prashant <pk021998@gmail.com>
2017-11-19 16:21:45 +0530

    :+1:

--- app/src/main/java/com/cbs/sscbs/LifeAtCbs.kt
+++ app/src/main/java/com/cbs/sscbs/LifeAtCbs.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.MediaController
 import android.widget.VideoView
-import kotlinx.android.synthetic.main.activity_life_at_cbs.*
 
 class LifeAtCbs : AppCompatActivity() {
 
@@ -15,13 +14,9 @@ class LifeAtCbs : AppCompatActivity() {
 
         val videoView = findViewById<VideoView>(R.id.playVideo) as VideoView
         videoView.setMediaController(MediaController(this))
-        videoView.setVideoURI(Uri.parse("https://github.com/Prashant-123/ImagesVideos/raw/master/Life%20At%20CBS%20New%20campus.mp4"))
+        videoView.setVideoURI(Uri.parse("https://github.com/Prashant-123/Stuff/blob/master/Life%20At%20CBS%20New%20campus%20-%20converted%20with%20Clipchamp.mp4?raw=true"))
         videoView.requestFocus()
-
-        PlayButton.setOnClickListener()
-        {
-            videoView.start()
-        }
+        videoView.start()
 
     }
 }
