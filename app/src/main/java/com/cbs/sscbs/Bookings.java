248e5cf877b420c7f5e7bbcc96d2cb95db321f43
Prashant <pk021998@gmail.com>
2017-12-22 22:50:48 +0530

    .

--- app/src/main/java/com/cbs/sscbs/Bookings.java
+++ app/src/main/java/com/cbs/sscbs/Bookings.java
@@ -1,17 +1,13 @@
 package com.cbs.sscbs;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -37,8 +33,9 @@ public class Bookings extends AppCompatActivity {
 
 
         recyclerView = (RecyclerView) findViewById(R.id.bookingRecycler);
-        final BookingsAdapter adapter = new BookingsAdapter(this, data);
+        final BookingsAdapter adapter = new BookingsAdapter(getApplicationContext(), data);
         recyclerView.setAdapter(adapter);
+
         BookingsDataClass newData1 = new BookingsDataClass("1", "2");
         BookingsDataClass newData2 = new BookingsDataClass("1", "2");
         BookingsDataClass newData3 = new BookingsDataClass("1", "2");
@@ -47,43 +44,43 @@ public class Bookings extends AppCompatActivity {
         data.add(newData3);
         adapter.notifyDataSetChanged();
 
-        database = FirebaseDatabase.getInstance();
-        databaseRef = database.getReference();
-
-
-        Intent intent = getIntent();
-        int child = intent.getIntExtra("childCount", 0);
-        for (int i=0; i<child; i++) {
-            DatabaseReference venue = database.getReference("EventThings").child(String.valueOf(i)).child("venue");
-            DatabaseReference time = database.getReference("EventThings").child(String.valueOf(i)).child("time");
-
-            venue.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    String v = dataSnapshot.getValue(String.class);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    System.out.println("The read failed: " + databaseError.getCode());
-                }
-            });
-
-            time.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    String t = dataSnapshot.getValue(String.class);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    System.out.println("The read failed: " + databaseError.getCode());
-                }
-            });
-
-
-        }
-
+//        database = FirebaseDatabase.getInstance();
+//        databaseRef = database.getReference();
+//
+//
+//        Intent intent = getIntent();
+//        int child = intent.getIntExtra("childCount", 0);
+//        for (int i=0; i<child; i++) {
+//            DatabaseReference venue = database.getReference("EventThings").child(String.valueOf(i)).child("venue");
+//            DatabaseReference time = database.getReference("EventThings").child(String.valueOf(i)).child("time");
+//
+//            venue.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    String v = dataSnapshot.getValue(String.class);
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//                    System.out.println("The read failed: " + databaseError.getCode());
+//                }
+//            });
+//
+//            time.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    String t = dataSnapshot.getValue(String.class);
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//                    System.out.println("The read failed: " + databaseError.getCode());
+//                }
+//            });
+//
+//
+//        }
+//
 
     }
 
