46c02fc479ebf1929603f9ddfd2785adf4a14413
Prashant <pk021998@gmail.com>
2017-12-23 01:36:57 +0530

    Digital Library Addded

--- app/src/main/java/com/cbs/sscbs/Contact_us.kt
+++ app/src/main/java/com/cbs/sscbs/Contact_us.kt
@@ -6,11 +6,6 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
-import android.text.method.LinkMovementMethod
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.support.v4.content.ContextCompat.startActivity
-import java.util.*
 
 
 class Contact_us : AppCompatActivity() {
@@ -40,13 +35,10 @@ class Contact_us : AppCompatActivity() {
             startActivity(Intent.createChooser(intent, "Send Email Using"))
         }
 
-        val txt = findViewById<View>(R.id.website) as TextView
-        txt.movementMethod = LinkMovementMethod.getInstance()
-        txt.setOnClickListener {
+        findViewById<View>(R.id.website).setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW)
             browserIntent.data = Uri.parse("http://www.sscbs.du.ac.in")
-            startActivity(browserIntent)
-        }
+            startActivity(browserIntent) }
 
         findViewById<View>(R.id.address).setOnClickListener {
             //val uri = String.format(Locale.ENGLISH, ":%f,%f", 28.7327, 77.1188)
