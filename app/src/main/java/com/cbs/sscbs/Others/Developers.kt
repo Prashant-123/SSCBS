1fcb9bab038cd85accb6686026b23ccf161076d9
Tanvi-Goyal <goyaltanvi94@gmail.com>
2018-01-24 02:29:19 +0530

    Developers Created

--- app/src/main/java/com/cbs/sscbs/Others/Developers.kt
+++ app/src/main/java/com/cbs/sscbs/Others/Developers.kt
@@ -1,10 +1,16 @@
 package com.cbs.sscbs.Others
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import com.cbs.sscbs.R
+import java.security.AccessController.getContext
 
 class Developers : AppCompatActivity() {
 
@@ -15,5 +21,22 @@ class Developers : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        val goToRepo = findViewById<TextView>(R.id.GoToRepo) as TextView
+
+        goToRepo.setOnClickListener(View.OnClickListener { view ->
+//            val btnAnimation = ButtonAnimation()
+//            btnAnimation.animateButton(view, applicationContext)
+            val uri = Uri.parse("https:/github.com/Prashant-123/SSCBS")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        })
     }
+}
+
+class ButtonAnimation {
+//    fun animateButton(v: View, context: Context) {
+//        val pressAnim = AnimationUtils.loadAnimation(context, R.anim.button_press_anim)
+//        v.startAnimation(pressAnim)
+//    }
 }
