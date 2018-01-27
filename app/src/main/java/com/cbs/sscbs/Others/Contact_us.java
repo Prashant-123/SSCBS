4435795007374152cc459512502cd3dca0ae36e8
Prashant <pk021998@gmail.com>
2018-01-28 03:09:09 +0530

    Manifest Fixed

--- app/src/main/java/com/cbs/sscbs/Others/Contact_us.java
+++ app/src/main/java/com/cbs/sscbs/Others/Contact_us.java
@@ -1,13 +1,13 @@
-package com.cbs.sscbs;
+package com.cbs.sscbs.Others;
 
 import android.content.Intent;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.cbs.sscbs.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,7 +19,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class Contact_us extends AppCompatActivity implements OnMapReadyCallback{
 
     GoogleMap map;
-    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
