abb0dbf844127bf7bf57dece88312562f5c20189
Prashant <pk021998@gmail.com>
2018-02-07 11:18:36 +0530

    3 Spinners Added

--- app/src/main/java/com/cbs/sscbs/Attendance/TeacherCourseDetails.java
+++ app/src/main/java/com/cbs/sscbs/Attendance/TeacherCourseDetails.java
@@ -1,5 +1,6 @@
 package com.cbs.sscbs.Attendance;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,11 +28,10 @@ import java.util.ArrayList;
 public class TeacherCourseDetails extends AppCompatActivity {
 
     private static final String TAG = "TAG";
-    private static final String Classes = "Classes";
-    String getClass;
-    String cls;
-    DataClas dataClas = new DataClas();
-    String getSub;
+    String getClass, getSub;
+    public int isLab;
+
+    String[] types;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,156 +109,8 @@ public class TeacherCourseDetails extends AppCompatActivity {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     }
-
                 });
-
-//        reference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {if (task.isSuccessful()) {
-//            for (DocumentSnapshot document : task.getResult()) {
-//                Log.wtf(TAG, document.getId() + " -> " + document.getData());
-//                            }
-//                        }
-//                    }
-//                });
-
-//        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(TeacherCourseDetails.this,
-//                                android.R.layout.simple_spinner_item, );
-//                        areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                        classSpinner.setAdapter(areasAdapter);
-//
-//        class_list.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//
-//                ArrayList<String> classesList = new ArrayList<>();
-//                if (task.isSuccessful()) {
-//                    for (DocumentSnapshot documentSnapshot : task.getResult()) {
-//                        Log.wtf(TAG,documentSnapshot.getId());
-//                        classesList.add(documentSnapshot.getId());
-//                        String[] classes = new String[classesList.size()+1];
-//                        int k = 0 ;
-//                        classes[0] = "Select Class";
-//                        for(int  i = 1 ; i <= classesList.size();i++){
-//                            classes[i] = classesList.get(k);
-//                            k++;
-//                        }
-//                        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(TeacherCourseDetails.this,
-//                                android.R.layout.simple_spinner_item, classes);
-//                        areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                        classSpinner.setAdapter(areasAdapter);
-//                        classSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//                            @Override
-//                            public void onItemSelected(final AdapterView<?> parentView, View selectedItemView, int position, long id) {
-//                                Log.wtf(TAG,"Selected");
-//                                if(position==0)
-//                                    Toast.makeText(TeacherCourseDetails.this, "Please select your class", Toast.LENGTH_SHORT).show();
-//                                else
-//                                    Toast.makeText(TeacherCourseDetails.this,
-//                                            "Selected " + parentView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
-//
-//                                CollectionReference sub_list = FirebaseFirestore.getInstance().
-//                                        collection("Teachers").document(getName).collection(Classes).
-//                                        document(parentView.getItemAtPosition(position).toString()).collection("Subjects");
-//
-//                                sub_list.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                                    @Override
-//                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                                        ArrayList<String> subList = new ArrayList<>();
-//                                        if(task.isSuccessful()){
-//                                            for(DocumentSnapshot documentSnapshot1 : task.getResult()){
-//                                                Log.wtf(TAG,documentSnapshot1.getId());
-//                                                subList.add(documentSnapshot1.getId());
-//
-//                                                Spinner subSpinner = (Spinner) findViewById(R.id.getSubSpinner);
-//                                                String[] sub = new String[subList.size()+1];
-//                                                int k = 0 ;
-//                                                sub[0] = "Select Subject";
-//                                                for(int  i = 1 ; i <= subList.size();i++){
-//                                                    sub[i] = subList.get(k);
-//                                                    k++;
-//                                                }
-//                                                ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(TeacherCourseDetails.this,
-//                                                        android.R.layout.simple_spinner_item, sub);
-//                                                areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                                                subSpinner.setAdapter(areasAdapter);
-//                                                subSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//                                                    @Override
-//                                                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                                                        CollectionReference type_list = FirebaseFirestore.getInstance().
-//                                                                collection("Teachers").document(getName).collection(Classes).
-//                                                                document(parentView.getItemAtPosition(position).toString()).
-//                                                                collection("Subjects").document(parent.getItemAtPosition(position).toString())
-//                                                                .collection("ClassType");
-//
-//                                                        type_list.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                                                            @Override
-//                                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                                                                ArrayList<String> typeList = new ArrayList<>();
-//                                                                if(task.isSuccessful()){
-//                                                                    for(DocumentSnapshot documentSnapshot2 :task.getResult()){
-//                                                                        Log.wtf(TAG,documentSnapshot2.getId());
-//                                                                        typeList.add(documentSnapshot2.getId());
-//
-//                                                                        Spinner typeSpinner = (Spinner) findViewById(R.id.getTypeSpinner);
-//                                                                        if(typeList.size()!=0) {
-//                                                                            typeSpinner.setVisibility(View.VISIBLE);
-//                                                                        }
-//                                                                        String[] type = new String[typeList.size()+1];
-//                                                                        int k = 0 ;
-//                                                                        type[0] = "Select Type ";
-//                                                                        for(int  i = 1 ; i <= typeList.size();i++){
-//                                                                            type[i] = typeList.get(k);
-//                                                                            k++;
-//                                                                        }
-//                                                                        ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(TeacherCourseDetails.this,
-//                                                                                android.R.layout.simple_spinner_item, type);
-//                                                                        areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                                                                        typeSpinner.setAdapter(areasAdapter);
-//                                                                        typeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//                                                                            @Override
-//                                                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                                                                                Intent intent = new Intent(getApplicationContext(),AttendanceMain.class);
-//                                                                                startActivity(intent);
-//                                                                            }
-//
-//                                                                            @Override
-//                                                                            public void onNothingSelected(AdapterView<?> parent) {
-//
-//                                                                            }
-//                                                                        });
-//                                                                    }
-//                                                                }
-//                                                            }
-//                                                        });
-//                                                    }
-//
-//                                                    @Override
-//                                                    public void onNothingSelected(AdapterView<?> parent) {
-//
-//                                                    }
-//                                                });
-//                                            }
-//                                        }
-//                                    }
-//                                });
-//
-//                            }
-//
-//                            @Override
-//                            public void onNothingSelected(AdapterView<?> parentView) {
-//                                // your code here
-//                            }
-//                        });
-//                    }
-//                } else {
-//                    Log.wtf(TAG, "Error getting teachers list");
-//                }
-//            }
-//        });
-
     }
-
     private void showSub(String getName, final Spinner subSpinner, final Spinner typeSpinner) {
         Log.wtf(TAG, getClass);
 
@@ -278,8 +130,8 @@ public class TeacherCourseDetails extends AppCompatActivity {
                     for (int i = 1; i <= subjectList.size(); i++) {
                         subjects[i] = subjectList.get(k);
                         k++;
-                        if (task.isSuccessful()) {
-                        }
+//                        if (task.isSuccessful()) {
+//                        }
                         ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(TeacherCourseDetails.this,
                                 android.R.layout.simple_spinner_item, subjects);
                         areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -297,18 +149,19 @@ public class TeacherCourseDetails extends AppCompatActivity {
                                     getSub = adapterView.getItemAtPosition(i).toString();
 
                                     CollectionReference subType = FirebaseFirestore.getInstance().collection("AllSubjects");
+                                    subType.whereEqualTo("type", true)
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
 
-                                    DocumentReference documentReference = subType.document(getSub);
-                                    documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                            if (task.isSuccessful()) {
-                                                DocumentSnapshot document = task.getResult();
-                                                if (document != null) {
-                                                    Log.d(TAG, "DocumentSnapshot data: " +
-                                                            task.getResult().getData().toString().substring(7, 29));
-
-                                                    String[] types = task.getResult().getData().toString().substring(7, 29).split(",");
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful())
+                                                        for (DocumentSnapshot document : task.getResult()) {
+                                                            if (getSub.equals(document.getId())) {
+                                                                types = new String[]{"Lab-G1", "Lab-G2", "Theory"};
+                                                            }
+                                                            else types = new String[]{"Theory"};
+                                                        }
 
                                                     ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(TeacherCourseDetails.this,
                                                             android.R.layout.simple_spinner_item, types);
@@ -323,6 +176,14 @@ public class TeacherCourseDetails extends AppCompatActivity {
                                                             else {
                                                                 Toast.makeText(TeacherCourseDetails.this,
                                                                         "Selected " + adapterView.getItemAtPosition(i), Toast.LENGTH_SHORT).show();
+
+                                                                //-----------INTENT-----------------
+
+                                                                Intent intent = new Intent(getApplicationContext(), AttendanceMain.class);
+                                                                startActivity(intent);
+
+                                                                //----------------------------------
+
                                                             }
                                                         }
 
@@ -331,16 +192,8 @@ public class TeacherCourseDetails extends AppCompatActivity {
 
                                                         }
                                                     });
-//
-
-                                                } else {
-                                                    Log.d(TAG, "No such document");
                                                 }
-                                            } else {
-                                                Log.d(TAG, "get failed with ", task.getException());
-                                            }
-                                        }
-                                    });
+                                            });
                                 }
                             }
 
